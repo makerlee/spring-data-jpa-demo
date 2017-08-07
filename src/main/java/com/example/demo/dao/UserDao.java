dce5f7024948bb0fb2c248d71ed0ee8beb511e04
jiyang.li <jiyang.li@timanetworks.com>
2017-08-07 18:15:55 +0800

    nothing

--- src/main/java/com/example/demo/dao/UserDao.java
+++ src/main/java/com/example/demo/dao/UserDao.java
@@ -1,12 +1,34 @@
 package com.example.demo.dao;
 
 import com.example.demo.domain.User;
+import org.jboss.jandex.Main;
 import org.springframework.stereotype.Repository;
+
+import java.lang.reflect.ParameterizedType;
 
 /**
  * Created by lijiyang on 2017/7/31.
  */
 @Repository
 public class UserDao extends HibernateBaseDao<User> {
+    private Class<User> entityClazz;
+
+    public Class<User> getEntityClazz() {
+        if (this.entityClazz == null) {
+            this.entityClazz = (Class<User>) ((ParameterizedType) getClass()
+                    .getGenericSuperclass()).getActualTypeArguments()[0];
+            logger.debug("T class = " + this.entityClazz.getCanonicalName());
+        }
+        return this.entityClazz;
+    }
+
+//    public static void main(String[] args) {
+//        UserDao userDao = new UserDao();
+//        String can = userDao.getEntityClazz().getCanonicalName();
+//        String name = userDao.getEntityClazz().getName();
+//
+//        System.out.println("--------->"+can);
+//        System.out.println("_________>"+name);
+//    }
 
 }
